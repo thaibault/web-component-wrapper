74fd67b65a0a1d0d581cb06c3b4424cdbcb98626
torben <t.sickert[~at~]gmail.com>
2020-08-30 18:19:14 +0200

    Automatic update to new version.

--- type.ts
+++ type.ts
@@ -16,108 +16,13 @@
     endregion
 */
 // region imports
+import PropertyTypes from 'clientnode/property-types'
 import {Mapping, ValueOf} from 'clientnode/type'
-import {
-    Component, FocusEvent, KeyUpEvent, MouseEvent, SyntheticEvent
-} from 'react'
 
-import PropertyTypes from './property-types'
-import {ReactWeb} from './web/ReactWeb'
+import {ReactWeb} from './adapter/ReactWeb'
 // endregion
 // region exports
-export type ModelState = {
-    dirty:boolean;
-    focused:boolean;
-    invalid:boolean;
-    invalidMaximum:boolean;
-    invalidMaximumLength:boolean;
-    invalidMinimum:boolean;
-    invalidMinimumLength:boolean;
-    invalidPattern:boolean;
-    invalidRequired:boolean;
-    pristine:boolean;
-    touched:boolean;
-    untouched:boolean;
-    valid:boolean;
-    visited:boolean;
-}
-export type BaseModel<Type = any> = {
-    declaration:string;
-    default:Type;
-    description:string;
-    editor:'code'|'code(css)'|'code(script)'|'plain'|'text'|'richtext(raw)'|'richtext(simple)'|'richtext(normal)'|'richtext(advanced)';
-    emtyEqualsNull:boolean;
-    maximum:number;
-    maximumLength:number;
-    minimum:number;
-    minimumLength:number;
-    name:string;
-    regularExpressionPattern:string;
-    selection:Array<number|string>|Mapping<number|string>;
-    trim:boolean;
-    type:'date'|'datetime-local'|'month'|'number'|'range'|'string'|'time'|'week';
-    value?:null|Type;
-}
-export type Model<Type = any> = BaseModel<Type> & {
-    mutable:boolean;
-    nullable:boolean;
-    state:ModelState;
-    writable:boolean;
-}
 export type Output = Mapping<true|((...parameter:Array<any>) => Mapping<any>)>
-export type Properties<Type = any> = BaseModel<Type> & ModelState & {
-    align:'end'|'start';
-    cursor:{
-        end:number;
-        start:number;
-    };
-    disabled?:boolean;
-    editorIsActive:boolean;
-    fullWidth:boolean;
-    icon:string|(IconOptions & {tooltip?:string|TooltipProps});
-    hidden:boolean;
-    maximumLengthText:string;
-    maximumText:string;
-    minimumLengthText:string;
-    minimumText:string;
-    model:Model<Type>;
-    onBlur:(event:SyntheticEvent) => void;
-    onChange:(properties:Properties<Type>, event?:SyntheticEvent) => void;
-    onChangeEditorIsActive:(isActive:boolean, event?:MouseEvent) => void;
-    onChangeValue:(value:null|Type, event:SyntheticEvent) => void;
-    onChangeShowDeclaration:(show:boolean, event:SyntheticEvent) => void;
-    onChangeState:(state:ModelState, event:SyntheticEvent) => void;
-    onClick:(event:MouseEvent) => void;
-    onConfigure:(properties:Properties<Type>) => void;
-    onFocus:(event:FocusEvent) => void;
-    onKeyUp:(event:KeyUpEvent) => void;
-    onTouch:(event:Event) => void;
-    outlined:boolean;
-    pattern:string;
-    patternText:string;
-    placeholder:string;
-    required?:boolean;
-    requiredText:string;
-    ripple:boolean;
-    rows:number;
-    selectableEditor:boolean;
-    showDeclaration:boolean;
-    showInitialValidationState:boolean;
-    tooltip:string|TooltipProps;
-    trailingIcon:string|(IconOptions & {tooltip?:string|TooltipProps});
-}
-export type Props<Type = any> = Partial<Properties<Type>>
-export type State<Type = any> = {
-    cursor:{
-        end:number;
-        start:number;
-    };
-    editorIsActive:boolean;
-    hidden:boolean;
-    model:ModelState;
-    showDeclaration:boolean;
-    value:null|Type;
-}
 export type WebComponentAPI = {
     component:ReactWeb;
     register:(tagName:string) => void;
